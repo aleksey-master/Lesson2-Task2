01ec46cd3afd319ad83986d2d6455b6d8a303a25
Aleksey <Aleksey-master@yandex.ru>
2022-08-10 20:46:37 +0400

    Study2Task2

--- Program.cs
+++ Program.cs
@@ -1,36 +1,17 @@
 ﻿//Напишите программу, которая выводит случайное трёхзначное число и удаляет вторую цифру этого числа.
 
-//int Prompt(string message)
-//{
-//    System.Console.WriteLine(message);              // Выводит сообщение
-//    string ReadValue = Console.ReadLine();          // Считывает с консоли строку
-//    int result = int.Parse(ReadValue);              // Преобразует строку в целое число
-//    return result;                                  // Возвращает результат функции
-//};
-//
-//int number = new Random().Next(10, 1000);
-//System.Console.WriteLine($"Случайное трехзначное число = {number}");
-//int number3 = number % 10;
-//int number2 = number / 10 % 10;
-//int number1 = number / 100 % 10;
-//
-//System.Console.WriteLine($"Число с удаленной второй цифрой = {number1}{number3}");
-
+int Prompt(string message)
+{
+    System.Console.WriteLine(message);              // Выводит сообщение
+    string ReadValue = Console.ReadLine();          // Считывает с консоли строку
+    int result = int.Parse(ReadValue);              // Преобразует строку в целое число
+    return result;                                  // Возвращает результат функции
+};
 
 int number = new Random().Next(10, 1000);
 System.Console.WriteLine($"Случайное трехзначное число = {number}");
+int number3 = number % 10;
+int number2 = number / 10 % 10;
+int number1 = number / 100 % 10;
 
-int number = Convert.ToInt32(Console.ReadLine());
-string numberText = Convert.ToString(number);
-if (numberText.Length > 2)
-{
-    Console.WriteLine("вторая цифра: " + numberText[1]);
-    System.Console.WriteLine();
-    
-}
-else
-{
-    Console.WriteLine("Tретьей цифры нет");
-}
-
-
+System.Console.WriteLine($"Число с удаленной второй цифрой = {number1}{number3}");
